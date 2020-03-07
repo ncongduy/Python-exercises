5b157df1f3b7cce02ba213e98ae120de7bf2211d
= <=>
2020-03-07 15:45:43 +0200

    modified Pomorodo file

--- Pomorodo.py
+++ Pomorodo.py
@@ -5,14 +5,14 @@ count = 1
 times_break = 5
 while count < times_break:
     print 'Begin ', count, 'Pomorodo', '- Current time: ', time.ctime()
-    webbrowser.open("https://www.youtube.com/watch?v=Tjbdg1emdzo")
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
     time.sleep(25*60)
     if count == 4:
         print 'Take a long break: 20 mitues'
         webbrowser.open('https://www.youtube.com/watch?v=I2X2SoLEK6E')
         break
     print 'Take a short break: 5 minutes'
-    webbrowser.open('https://www.youtube.com/watch?v=Tjbdg1emdzo')
+    webbrowser.open('http://www.youtube.com/watch?v=dQw4w9WgXcQ')
     time.sleep(5*60)
     count += 1
 
